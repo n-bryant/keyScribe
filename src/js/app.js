fa0bb6118ece64789ab3da93e2740bb9ff49f41d
Nicholas Bryant <nicholas@Nicholass-MacBook-Pro.local>
2017-04-25 12:23:06 -0400

    built out layout of piano

--- src/js/app.js
+++ src/js/app.js
@@ -3,21 +3,25 @@
 /* VexTab */
 const notationContainer = document.querySelector('.notation-container');
 const Renderer = Vex.Flow.Renderer;
-
 const renderer = new Renderer(notationContainer, Renderer.Backends.SVG);
 
 // Initialize VexTab artist and parser.
 const artist = new Artist(10, 10, 600, {scale: 0.8});
 const vextab = new VexTab(artist);
 
-try {
-    // Parse VexTab music notation passed in as a string.
-    vextab.parse("tabstave notation=true clef=treble key=F time=4/4 tablature=false\n notes :16(C/4.E/4.G/4):8C/4E/4sG/4|:qsC/4:8E/4F/4##|:hdD/4:qG/4");
+let clef = 'treble';
+let keySig = 'C';
+let timeSig = '4/4';
+let vexString = `tabstave notation=true clef=${clef} key=${keySig} time=${timeSig} tablature=false\n notes :16(C/4.E/4.G/4):8C/4E/4sG/4|:qsC/4:8C#/4F/4##|:hdD/4:qG/4`;
 
-    // Render notation onto canvas.
-    artist.render(renderer);
+try {
+  // Parse VexTab music notation passed in as a string.
+  vextab.parse(vexString);
+
+  // Render notation onto canvas.
+  artist.render(renderer);
 } catch (e) {
-    console.log(e);
+  console.log(e);
 }
 
 /* ToneJS */
@@ -26,3 +30,4 @@ var synth = new Tone.Synth().toMaster();
 
 //play a middle 'C' for the duration of an 8th note
 // synth.triggerAttackRelease("C4", "8n");
+const keys = document.querySelectorAll('.key');
